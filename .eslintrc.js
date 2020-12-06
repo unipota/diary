42a9d1ff62e563367aaa38f2b677cc17b8bef6b4
unipota <contact@unipota.me>
2020-12-07 00:21:49 +0900

    :truck: rename post pass

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     },
   },
   rules: {
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
