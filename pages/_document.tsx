83a959b8c6239bba5d31c4c1ee01648acd9657bc
unipota <contact@unipota.me>
2021-03-25 00:43:58 +0900

    remote gtag on dev

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,15 @@ export default class MyDocument extends Document {
     return (
       <Html lang="ja">
         <Head>
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
+          {GA_TRACKING_ID !== undefined ?? (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
@@ -20,8 +22,10 @@ export default class MyDocument extends Document {
               page_path: window.location.pathname,
             });
           `,
-            }}
-          />
+                }}
+              />
+            </>
+          )}
         </Head>
         <body>
           <Main />
